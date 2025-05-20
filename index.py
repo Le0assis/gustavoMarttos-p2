d5aafc96081c44dcbc600993b83a666ccdfad0fc
LEONARDO RIBEIRO DE ASSIS <leoribeiroassis@gmail.com>
2025-05-20 10:46:39 -0300

    Commit Inicial

---
+++ index.py
@@ -0,0 +1,43 @@
+materia ={
+    "01":{
+        "materia": "matematica",
+        "cargahoraria": 40,
+        "professor": "Mauricio",
+    },
+    "02":{
+        "materia": "portugues",
+        "cargahorario": 55,
+        "professor": "prof",  
+    },
+    "03":{
+        "materia": "quimica",
+        "cargahoraria": 40,
+        "professor": "prof",
+    },
+}
+
+alunos = {
+    "01":{
+        "nome": "Adriano",
+        "idade": 13,
+        "serie": 6,
+        "periodo": "Manhã"
+    },
+    "02":{
+        "nome": "Bianca" ,
+        "idade": 13 ,
+        "serie": 7,
+        "periodo": "Manhã",
+    },
+    "02":{
+        "nome": "Conceição" ,
+        "idade": 12,
+        "serie": 6,
+        "periodo": "Tarde",
+    },
+    
+    # Listagem: curso e alunos
+    # Adicionar: alunos e cursos
+    # atribuir cursos a alunos
+    # citar cursos de um aluno
+}
